eeca7be4b7573676fcf18a39fb79935df4f92c8f
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-11-12 23:21:52 +0530

    commits

---
+++ Python-Codes/dataTypes/DataTypes.py
@@ -0,0 +1,11 @@
+a = 1 # This is Integer
+
+b = 2.2 # This is Floating Number
+
+c ="CodeCshekhar" # This is String
+
+d = False # This is Boolean
+
+e = None # This is just None Type Variable
+
+print(a,b,c,d,e)
