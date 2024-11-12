d0776e0f35ef0bdd8771cb9208cd7e73266ff993
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-11-12 23:35:13 +0530

    commits

---
+++ Python-Codes/operators/Arithemetic.py
@@ -0,0 +1,30 @@
+# Arithmetic Operators
+
+# Addition
+a = 1
+b = 2
+c = a + b
+print(c)
+
+#------------
+# Subtraction
+c = 2
+d = 1
+e = c - d
+print(e)
+
+#------------
+# Multiplication
+f = 1
+g = 2
+h = f*g
+print(h)
+
+#------------
+# Divide
+
+i = 2
+j = 1
+k = i/j
+print(k)
+
