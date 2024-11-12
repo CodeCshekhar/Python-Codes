e180460eca5a3a615b3da3f1def8fa5db5f47a92
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-11-12 23:35:47 +0530

    commits

--- Python-Codes/operators/Arithemetic.py
+++ Python-Codes/operators/Arithemetic.py
@@ -1,4 +1,3 @@
-# Arithmetic Operators
 
 # Addition
 a = 1
@@ -6,23 +5,19 @@ b = 2
 c = a + b
 print(c)
 
-#------------
 # Subtraction
 c = 2
 d = 1
 e = c - d
 print(e)
 
-#------------
 # Multiplication
 f = 1
 g = 2
 h = f*g
 print(h)
 
-#------------
 # Divide
-
 i = 2
 j = 1
 k = i/j
