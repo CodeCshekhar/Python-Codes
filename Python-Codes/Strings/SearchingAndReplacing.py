6ab061c7062829f1c460474ffa5f92d23de71d6a
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-12-06 13:11:50 +0530

    Commit

---
+++ Python-Codes/Strings/SearchingAndReplacing.py
@@ -0,0 +1,7 @@
+s = "hello world"
+print(s.find('world'))        # 6
+print(s.index('world'))       # 6
+print(s.rfind('o'))           # 7
+print(s.rindex('o'))          # 7
+print(s.replace('world', 'Python'))  # hello Python
+print(s.count('o'))           # 2
