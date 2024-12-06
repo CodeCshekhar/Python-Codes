247f348fc1e1b2ad2ec301155a6362ed349a0fdf
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-12-06 12:44:26 +0530

    Commit

--- Python-Codes/Strings/QuotedAndStringSlicing.py
+++ Python-Codes/Strings/QuotedAndStringSlicing.py
@@ -5,5 +5,6 @@ r =  """Demo3""" # Triple Quoted String
 
 print(s+t+r) #Simply printing All
 
+#We are Cutting the String in simple language
 slice = s[0:1] #Staring from index zero to one
 print(slice)
