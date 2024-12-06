ce5327566aff29372de379c63a7dc88617460b63
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-12-06 12:55:40 +0530

    Commit

--- Python-Codes/Strings/StringSlicing.py
+++ Python-Codes/Strings/StringSlicing.py
@@ -1,11 +1,10 @@
-
 s = 'Demo' # Single Quoted String
 t = '"Demo2"' # Double Quoted String
 r =  """Demo3""" # Triple Quoted String
-
 print(s+t+r) #Simply printing All
 
 #We are Cutting the String in simple language
 slice = s[0:1] #Staring from index zero to one
 slice1 = t[-3:-1] #Staring from index minus three to minus one - Negative Slicing
-print(slice)
+slice2 = r[0:4:2] #Slicing with values skip
+print(slice + slice1 + slice2)
