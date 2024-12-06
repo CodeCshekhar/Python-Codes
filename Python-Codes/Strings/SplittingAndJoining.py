55c481bdf8f455a5e40d3e2f67937785641697df
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-12-06 13:11:56 +0530

    Commit

---
+++ Python-Codes/Strings/SplittingAndJoining.py
@@ -0,0 +1,5 @@
+s = "apple,banana,cherry"
+print(s.split(','))           # ['apple', 'banana', 'cherry']
+print(s.rsplit(',', 1))       # ['apple,banana', 'cherry']
+print("line1\nline2".splitlines())  # ['line1', 'line2']
+print('-'.join(['a', 'b', 'c']))    # a-b-c
