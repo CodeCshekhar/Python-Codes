4e5800308be6732723d58879794363e9b8670f7b
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-12-06 12:43:42 +0530

    Commit

---
+++ Python-Codes/Strings/QuotedAndStringSlicing.py
@@ -0,0 +1,9 @@
+
+s = 'Demo' # Single Quoted String
+t = '"Demo2"' # Double Quoted String
+r =  """Demo3""" # Triple Quoted String
+
+print(s+t+r) #Simply printing All
+
+slice = s[0:1] #Staring from index zero to one
+print(slice)
