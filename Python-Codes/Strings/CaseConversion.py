1a0ed7bbf7692b842c6c546e1d57f522fd655213
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-12-06 13:11:12 +0530

    Commit

---
+++ Python-Codes/Strings/CaseConversion.py
@@ -0,0 +1,7 @@
+s = "hello world"
+print(s.capitalize())  # Hello world
+print(s.casefold())    # hello world
+print(s.lower())       # hello world
+print(s.upper())       # HELLO WORLD
+print(s.swapcase())    # HELLO WORLD
+print(s.title())       # Hello World
